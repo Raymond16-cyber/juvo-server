33c5feb319bc34c652b97497a22f4817ca9567ce
Raymond <uchennaraymond74@gmail.com>
2026-09-03 17:22:42 +0100

    updated analytics feature

--- config/db.js
+++ config/db.js
@@ -4,9 +4,14 @@ import mongoose from "mongoose";
 
 
 const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/juvo";
+const isProduction = process.env.NODE_ENV === "production";
+const mongoUrl = process.env.MONGO_URL || "mongodb+srv://Raymond17:2xGjtsHCqigXSpzc@clustergomycode.7pynx6y.mongodb.net/juvo?retryWrites=true&w=majority";
 export default async function connectDb() {
   mongoose.set("strictQuery", true);
-  await mongoose.connect(mongoUri);
+  await mongoose.connect(isProduction ? mongoUrl : mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
   try {
     const { default: Journal } = await import("../models/Journal.js");
